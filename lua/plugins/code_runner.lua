fd7b521e6269595aed64f29e11598d48a67db551
arthur-x300 <aaa890177@gmail.com>
2023-09-05 00:18:52 +0800

    update

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -16,7 +16,7 @@ end
 function M.config()
     require('code_runner').setup({
         -- choose default mode (valid term, tab, float, toggle)
-        mode = "tab",
+        mode = "float",
         --mode = "term",
         -- Focus on runner window(only works on toggle, term and tab mode)
         focus = true,
@@ -30,19 +30,24 @@ function M.config()
             size = 12,
         },
         float = {
-            close_key = "<ESC>",
+            close_key = "<C-c>",
             -- Window border (see ':h nvim_open_win')
-            border = "none",
-
+                    --• "none": No border (default).
+                    --• "single": A single line box.
+                    --• "double": A double line box.
+                    --• "rounded": Like "single", but with rounded corners ("╭" etc.).
+                    --• "solid": Adds padding by a single whitespace cell.
+                    --• "shadow": A drop shadow effect by blending with the background.
+            border = "rounded",
             -- Num from `0 - 1` for measurements
-            height = 0.8,
+            height = 0.3,
             width = 0.8,
-            x = 0.5,
-            y = 0.5,
+            x = 0,
+            y = 0.92,
 
             -- Highlight group for floating window/border (see ':h winhl')
-            border_hl = 'Normal',
-            float_hl = 'winblend',
+            border_hl = 'FloatBorder',
+            float_hl = 'Normal',
 
             -- Transparency (see ':h winblend')
             blend = 0,
@@ -51,7 +56,7 @@ function M.config()
             clean = false, -- Clean terminal before launch
             number = 10, -- Use nil for dynamic number and set init
             init = nil,
-        },   
+        },
         filetype_path = "",
         -- Execute before executing a file
         before_run_filetype = function()
