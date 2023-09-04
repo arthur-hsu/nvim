a2468dc851af406d786f1c1b98bdd88df1b41cef
Arthur Hsu <aaa890177@gmail.com>
2023-09-04 13:13:48 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,16 +16,16 @@ local keymap = vim.api.nvim_set_keymap
 if vim.loop.os_uname().sysname == 'Linux' then
     vim.opt.shell = 'bash'
     keymap("n", "rc",":Telescope file_browser path=$HOME/.config/nvim/lua<CR>" ,opts)
-    keymap("c", "NOTE", ":e $HOME/Desktop/note.txt<CR>",opts)
-    keymap("c", "TEST", ":e $HOME/Desktop/test.py<CR>",opts)
+    keymap("c", "Note", ":e $HOME/Desktop/note.txt<CR>",opts)
+    keymap("c", "Test", ":e $HOME/Desktop/test.py<CR>",opts)
     --keymap("n", "<F2>",":terminal bash<CR>i", opts)
 elseif vim.loop.os_uname().sysname == 'Windows_NT' then
     vim.opt.shell = 'pwsh'
     vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
     vim.opt.shellxquote = ''
     keymap("n", "rc",":Telescope file_browser path=$HOME\\AppData\\Local\\nvim\\lua<CR>" ,opts)
-    keymap("c", "NOTE", ":e $HOME\\Desktop\\note.txt<CR>",opts)
-    keymap("c", "TEST", ":e $HOME\\Desktop\\test.py<CR>",opts)
+    keymap("c", "Note", ":e $HOME\\Desktop\\note.txt<CR>",opts)
+    keymap("c", "Test", ":e $HOME\\Desktop\\test.py<CR>",opts)
     --keymap("n", "<F2>",":terminal pwsh<CR>i", opts)
 end
 
