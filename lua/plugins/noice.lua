0176bc1439c56ee77a9d4725b3f5888df7be426a
arthur <aaa890177@gmail.com>
2023-09-12 15:20:51 +0800

    up

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -144,12 +144,12 @@ function M.config()
                 opts = {}, -- merged with defaults from documentation
             },
             signature = {
-                enabled = false,
+                enabled = true,
                 auto_open = {
                     enabled = true,
                     trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
                     luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-                    throttle = 50, -- Debounce lsp signature help request by 50ms
+                    throttle = 10, -- Debounce lsp signature help request by 50ms
                 },
                 view = nil, -- when nil, use defaults from documentation
                 ---"@type NoiceViewOptions"
