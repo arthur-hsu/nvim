f4f994006b91530eac4dc5f4c19dd6474c160516
arthur-t14 <aaa890177@gmail.com>
2023-09-20 11:41:31 +0800

    up

--- init.lua
+++ init.lua
@@ -1,12 +1,18 @@
-require "config.diagnostic"
-require "config.options"
-require "config.lazy"
-require "config.keymaps"
-require "config.lualine_theme"
---vim.cmd.colorscheme "kanagawa"
---vim.cmd.colorscheme "darkplus"
-vim.cmd.colorscheme "zephyr"
---vim.cmd.colorscheme "tokyonight"
+if vim.g.vscode then
+    -- VSCode extension
+    require "config.options"
+else
+    -- ordinary Neovim
+    require "config.diagnostic"
+    require "config.lazy"
+    require "config.keymaps"
+    require "config.lualine_theme"
+    --vim.cmd.colorscheme "kanagawa"
+    --vim.cmd.colorscheme "darkplus"
+    vim.cmd.colorscheme "zephyr"
+    --vim.cmd.colorscheme "tokyonight"
+end
+
 
 
 
