22e6938ba1d858ec828d28835d4afe9640356cb0
arthur-mac <aaa890177@gmail.com>
2023-10-13 01:24:15 +0800

    update path

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 sudo apt-get install nodejs -y
 
 
-sudo apt-get install -y ninja-build gettext cmake unzip curl;
+sudo apt-get install -y ninja-build gettext cmake unzip curl bpytop;
 sudo apt install -y ripgrep fd-find bat tldr exa xclip
 git clone https://github.com/neovim/neovim /tmp/neovim;
 cd /tmp/neovim;
@@ -17,11 +17,14 @@ git checkout stable;
 make CMAKE_BUILD_TYPE=RelWithDebInfo;
 sudo make install
 
+
 sudo pip3 install neovim
 sudo npm install neovim
+
 sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-chsh -s $(which zsh)
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cd
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#chsh -s $(which zsh)
+#git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
