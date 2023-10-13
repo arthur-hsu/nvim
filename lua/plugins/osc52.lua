e436bca57bb1e991d9fe8c8c5a0a3cd9275029e7
arthur-deskpi <aaa890177@gmail.com>
2023-10-14 01:56:23 +0800

    up

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -1,6 +1,6 @@
 local M = {
-  'ojroques/nvim-osc52',
-  event="VeryLazy",
+    'ojroques/nvim-osc52',
+    event="VeryLazy",
 }
 
 function M.config()
@@ -8,7 +8,6 @@ function M.config()
         silent     = true,
     }
 
-    vim.keymap.set('v', '<leader>y', require('osc52').copy_visual)
     --Using nvim-osc52 as clipboard provider
     local function copy(lines, _)
       require('osc52').copy(table.concat(lines, '\n'))
@@ -25,17 +24,17 @@ function M.config()
     }
     local opts = {noremap = false, silent = true }
 	if vim.g.clipboard.name == 'osc52' then
-	    vim.keymap.set("v", "<C-c>", require('osc52').copy_visual,opts)
-	    vim.keymap.set("v", "y", require('osc52').copy_visual,opts)
+	    vim.keymap.set("v", "<C-c>", require('osc52').copy_visual, opts)
+	    vim.keymap.set("v", "y", require('osc52').copy_visual, opts)
 	    if vim.loop.os_uname().sysname == 'Darwin' then
             vim.keymap.set("v", "M-c",require('osc52').copy_visual,opts)
 	    end
-	else
-	    vim.keymap.set("v", "<C-c>", '"+y', opts)
-	    vim.keymap.set("v", "y", '"+y', opts)
-	    if vim.loop.os_uname().sysname == 'Darwin' then
-            vim.keymap.set("v", "<M-c>", '"+y', opts)
-	    end
+	--else
+		--vim.keymap.set("v", "<C-c>", '"+y', opts)
+		--vim.keymap.set("v", "y", '"+y', opts)
+		--if vim.loop.os_uname().sysname == 'Darwin' then
+            --vim.keymap.set("v", "<M-c>", '"+y', opts)
+		--end
 	end
 end
 return M
