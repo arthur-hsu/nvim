43783562c5ec2004fba20e965878c840c016f6c2
arthur-deskpi <aaa890177@gmail.com>
2023-10-14 10:43:29 +0800

    update

--- lua/config/lualine_theme.lua
+++ lua/config/lualine_theme.lua
@@ -38,8 +38,8 @@ local config = {
     options = {
         -- Disable sections and component separators
         disabled_filetypes = {     -- Filetypes to disable lualine for.
-            statusline = {"NvimTree","diffpanel"},      -- only ignores the ft for statusline.
-            winbar = {"NvimTree","diffpanel"},          -- only ignores the ft for winbar.
+            statusline = {"NvimTree","diffpanel",'alpha'},      -- only ignores the ft for statusline.
+            winbar = {"NvimTree","diffpanel",'alpha'},          -- only ignores the ft for winbar.
         },
         globalstatus = true,
         component_separators = '',
