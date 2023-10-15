91cb2b544f56e9ad2339b458c8195725f2fb4618
arthur-mac <aaa890177@gmail.com>
2023-10-15 13:35:30 +0800

    update

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,78 +1,86 @@
--- Header
-
-local function header()
-    local arthur = {
-        " ,.  ,-.  ,---. .  . .  . ,-.  ",
-        "/  \\ |  )   |   |  | |  | |  ) ",
-        "|--| |-<    |   |--| |  | |-<  ",
-        "|  | |  \\   |   |  | |  | |  \\ ",
-        "'  ' '  '   '   '  ' `--` '  ' ",
-    }
-    local arthur_1 = {
-    " ▄▄▄       ██▀███  ▄▄▄█████▓ ██░ ██  █    ██  ██▀███  ",
-    "▒████▄    ▓██ ▒ ██▒▓  ██▒ ▓▒▓██░ ██▒ ██  ▓██▒▓██ ▒ ██▒",
-    "▒██  ▀█▄  ▓██ ░▄█ ▒▒ ▓██░ ▒░▒██▀▀██░▓██  ▒██░▓██ ░▄█ ▒",
-    "░██▄▄▄▄██ ▒██▀▀█▄  ░ ▓██▓ ░ ░▓█ ░██ ▓▓█  ░██░▒██▀▀█▄  ",
-    " ▓█   ▓██▒░██▓ ▒██▒  ▒██▒ ░ ░▓█▒░██▓▒▒█████▓ ░██▓ ▒██▒",
-    " ▒▒   ▓▒█░░ ▒▓ ░▒▓░  ▒ ░░    ▒ ░░▒░▒░▒▓▒ ▒ ▒ ░ ▒▓ ░▒▓░",
-    "  ▒   ▒▒ ░  ░▒ ░ ▒░    ░     ▒ ░▒░ ░░░▒░ ░ ░   ░▒ ░ ▒░",
-    "  ░   ▒     ░░   ░   ░       ░  ░░ ░ ░░░ ░ ░   ░░   ░ ",
-    "      ░  ░   ░               ░  ░  ░   ░        ░     ",
-    --"                                                      ",
-    }
-	return arthur_1
-end
--- Footer
-local function footer()
-	local version = vim.version()
-	local print_version = "v" .. version.major .. "." .. version.minor .. "." .. version.patch
-	local datetime = os.date("%Y/%m/%d %H:%M:%S")
-
-	return {
-		--" ",
-		print_version .. " - " .. datetime,
-	}
-end
-
 return {
     "goolord/alpha-nvim",
     event = "VimEnter",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local alpha = require("alpha")
+    --lazy = false,
+    opts = function()
         local dashboard = require("alpha.themes.dashboard")
+        local logo = [[
+           ▄ ▄
+       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄
+       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █
+    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █
+  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄
+▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █
+█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █
+    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█
+]]
+        local arthur_1 = [[
+ ▄▄▄       ██▀███  ▄▄▄█████▓ ██░ ██  █    ██  ██▀███  
+▒████▄    ▓██ ▒ ██▒▓  ██▒ ▓▒▓██░ ██▒ ██  ▓██▒▓██ ▒ ██▒
+▒██  ▀█▄  ▓██ ░▄█ ▒▒ ▓██░ ▒░▒██▀▀██░▓██  ▒██░▓██ ░▄█ ▒
+░██▄▄▄▄██ ▒██▀▀█▄  ░ ▓██▓ ░ ░▓█ ░██ ▓▓█  ░██░▒██▀▀█▄  
+ ▓█   ▓██▒░██▓ ▒██▒  ▒██▒ ░ ░▓█▒░██▓▒▒█████▓ ░██▓ ▒██▒
+ ▒▒   ▓▒█░░ ▒▓ ░▒▓░  ▒ ░░    ▒ ░░▒░▒░▒▓▒ ▒ ▒ ░ ▒▓ ░▒▓░
+  ▒   ▒▒ ░  ░▒ ░ ▒░    ░     ▒ ░▒░ ░░░▒░ ░ ░   ░▒ ░ ▒░
+  ░   ▒     ░░   ░   ░       ░  ░░ ░ ░░░ ░ ░   ░░   ░ 
+      ░  ░   ░               ░  ░  ░   ░        ░     
+        ]]
 
-        dashboard.section.header.val = header()
-        dashboard.section.footer.val = footer()
-
-        -- Menu
-        -- TODO: Add projects and Frecency?
+        dashboard.section.header.val = vim.split(arthur_1, "\n")
         dashboard.section.buttons.val = {
-            dashboard.button("e",  "  New file", ":ene <BAR> startinsert<CR>"),
-            dashboard.button("t",  "  Terminal", "<cmd>terminal<CR>i"),
-            dashboard.button("d",  "  Diffview", "<cmd>DiffviewFileHistory<CR>"),
-            dashboard.button("r", "󰔠  Recent files", "<cmd>Telescope oldfiles<CR>"),
-            --dashboard.button("FB", "  File browser", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>"),
-            --dashboard.button("FG", "󰊄  Find text", "<cmd>Telescope live_grep<CR>"),
-            --dashboard.button("FP", "󱠏  Projects", "<cmd>lua require'telescope'.extensions.project.project{}<CR>"),
-            --dashboard.button("rc", "  Config", "<cmd>e $MYVIMRC<CR>"),
-            --dashboard.button("L",  "  Plugins", "<cmd>Lazy<CR>"),
-            dashboard.button("q",  "  Quit", "<cmd>qa<CR>"),
+            dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+            dashboard.button("n", "󰙴 " .. " New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+            dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+            dashboard.button("e", " " .. " Explore", ":Telescope file_browser<CR>"),
+            dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+            dashboard.button("q", " " .. " Quit", ":qa<CR>"),
         }
 
-        alpha.setup(dashboard.config)
+        -- set highlight
+        --for _, button in ipairs(dashboard.section.buttons.val) do
+            --button.opts.hl = "AlphaButtons"
+            --button.opts.hl_shortcut = "AlphaShortcut"
+        --end
+        --dashboard.section.header.opts.hl = "comment"
+        --dashboard.section.buttons.opts.hl = "AlphaButtons"
+        --dashboard.section.footer.opts.hl = "AlphaFooter"
+        --dashboard.opts.layout[1].val = 10
+        return dashboard
+    end,
+    config = function(_, dashboard)
+        -- close Lazy and re-open when the dashboard is ready
+        if vim.o.filetype == "lazy" then
+            vim.cmd.close()
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "AlphaReady",
+                callback = function()
+                    require("lazy").show()
+                end,
+            })
+        end
+
+        require("alpha").setup(dashboard.opts)
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "LazyVimStarted",
+            callback = function()
+                local stats = require("lazy").stats()
+                local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                local version = "  󰥱 v"
+                    .. vim.version().major
+                    .. "."
+                    .. vim.version().minor
+                    .. "."
+                    .. vim.version().patch
+                local plugins = "⚡ loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+                local datetime = os.date("        %m/%d %a          %H:%M")
+                local footer = version .. "\t" .. plugins .. "\n \n" .. datetime .. "\n"
+                dashboard.section.footer.val = footer
+                pcall(vim.cmd.AlphaRedraw)
+            end,
+        })
     end,
 }
---return{
-    --{
-        --'goolord/alpha-nvim',
-        ----enabled = false,
-        --lazy=false,
-        --ement = "VimEnter",
-        --dependencies = { 'nvim-tree/nvim-web-devicons' },
-        --config = function ()
-            ----require'alpha'.setup(require'alpha.themes.startify'.config)
-            --require'alpha'.setup(require'alpha.themes.dashboard'.config)
-        --end
-    --}
---}
