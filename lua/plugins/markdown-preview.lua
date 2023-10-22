30d98be761e3f8979bf55bb09f8d089c33ee32fe
arthur-mac <aaa890177@gmail.com>
2023-10-22 23:40:53 +0800

    merge gwmfg

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -5,7 +5,7 @@ return{
     --build = "cd app && npm install",
     --build = function() vim.fn["mkdp#util#install"]() end,
     config = function()
-        --vim.fn["mkdp#util#install"]()
+        vim.fn["mkdp#util#install"]()
         vim.keymap.set('n', '<leader>md', "<CMD>MarkdownPreview<CR>", { noremap = true, silent = true })
         vim.g.mkdp_auto_close = true
         vim.g.mkdp_open_to_the_world = false
@@ -15,10 +15,10 @@ return{
         vim.g.mkdp_echo_preview_url = false
         vim.g.mkdp_page_title = "${name}"
         vim.g.mkdp_theme = 'dark'
-        local install_path = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app"
-        local node_modules = install_path .. "/node_modules"
-        if vim.fn.empty(vim.fn.glob(node_modules)) > 0 then
-            vim.cmd("!cd " .. install_path .. " && npm install")
-        end
+        --local install_path = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app"
+        --local node_modules = install_path .. "/node_modules"
+        --if vim.fn.empty(vim.fn.glob(node_modules)) > 0 then
+            --vim.cmd("!cd " .. install_path .. " && npm install")
+        --end
     end,
 }
