df4ff00f3922e27b6227869012de692e50c86bc0
arthur-mac <aaa890177@gmail.com>
2023-10-27 12:45:46 +0800

    update readme

--- shell/setup.sh
+++ shell/setup.sh
@@ -10,23 +10,14 @@ sudo apt-get update
 sudo apt-get install nodejs -y
 
 
-# sudo apt-get install -y ninja-build gettext cmake unzip curl bpytop;
-# sudo apt install -y ripgrep fd-find bat tldr exa xclip
-# git clone https://github.com/neovim/neovim /tmp/neovim;
-# cd /tmp/neovim;
-# git checkout stable;
-# make CMAKE_BUILD_TYPE=RelWithDebInfo;
-# sudo make install
+sudo apt-get install -y ninja-build gettext cmake unzip curl bpytop;
+sudo apt install -y ripgrep fd-find bat tldr exa xclip
+git clone https://github.com/neovim/neovim /tmp/neovim;
+cd /tmp/neovim;
+git checkout stable;
+make CMAKE_BUILD_TYPE=RelWithDebInfo;
+sudo make install
 
 
-#sudo pip3 install neovim
-#sudo npm install neovim
-
-#sudo apt install -y zsh
-#cd ~
-#exit
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#chsh -s $(which zsh)
-#git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo pip3 install neovim
+sudo npm install neovim
--- shell/zsh.sh
+++ shell/zsh.sh
@@ -3,7 +3,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-chsh -s $(which zsh)
 echo 'export EDITOR="nvim"' |tee -a ~/.zshrc | tee -a ~/.zshrc
 echo 'alias nv="nvim"' | tee -a ~/.zshrc
 echo 'alias vim="nvim"' | tee -a ~/.zshrc
@@ -20,4 +19,4 @@ echo 'alias dc="docker-compose"' | tee -a ~/.zshrc
 echo 'alias drmall="#docker stop $(docker ps -a -q) ; #docker rm -f $(docker ps -a -q); docker rmi -f $(docker images -q)"' | tee -a ~/.zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting sudo)/g' ~/.zshrc
 echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' | tee -a ~/.zshrc
-
+chsh -s $(which zsh)
