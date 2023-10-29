99a9495b7a8a0f094db5d3eb9ccbfcca73360bd0
arthur-mac <aaa890177@gmail.com>
2023-10-29 18:54:28 +0800

    add ufo

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -40,7 +40,7 @@ return {
             dashboard.button("s", " " .. " Current folder session", [[:lua require("persistence").load() <cr>]]),
             dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
             dashboard.button("n", "󰙴 " .. " New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+            dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
             dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
             dashboard.button("q", " " .. " Quit", ":qa<CR>"),
             --dashboard.button("e", " " .. " Explore", ":Telescope file_browser<CR>"),
