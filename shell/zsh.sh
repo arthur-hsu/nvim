d9639e67438f52c38e8c09ca15c9d945c7b166e5
arthur-mac <aaa890177@gmail.com>
2023-10-30 21:25:47 +0800

    merge

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -1,6 +1,9 @@
 sudo apt install -y zsh
-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# 安装 Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# 将 Zsh 设置为默认 Shell
+chsh -s $(which zsh)
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -19,5 +22,14 @@ echo 'alias d="docker"' | tee -a ~/.zshrc
 echo 'alias dc="docker-compose"' | tee -a ~/.zshrc
 echo 'alias drmall="#docker stop $(docker ps -a -q) ; #docker rm -f $(docker ps -a -q); docker rmi -f $(docker images -q)"' | tee -a ~/.zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting sudo)/g' ~/.zshrc
-echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' | tee -a ~/.zshrc
-chsh -s $(which zsh)
+
+original="ZSH_THEME=\".*\""
+new="ZSH_THEME=\"powerlevel10k/powerlevel10k\""
+# 检查.zshrc文件是否存在，如果存在则进行替换操作
+if [ -f ~/.zshrc ]; then
+    # 使用sed命令将ZSH_THEME更改为新值
+    sed -i '' -E "s/$original/$new/" ~/.zshrc
+    echo "ZSH_THEME updated to powerlevel10k/powerlevel10k in .zshrc"
+else
+    echo "File ~/.zshrc not found. Please make sure the file exists."
+fi
