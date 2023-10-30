c970474e7df2e82f129d2d90d4c0b658f16ece1b
arthur-mac <aaa890177@gmail.com>
2023-10-30 23:34:14 +0800

    merge

--- shell/setup.sh
+++ shell/setup.sh
@@ -20,6 +20,7 @@ sudo apt-get install nodejs -y
 sudo apt-get install -y ninja-build gettext cmake unzip curl bpytop;
 sudo apt install -y ripgrep fd-find bat tldr exa xclip
 
+cd ~/.config/nvim/shell/
 system_=$(uname -m)
 
 if [ "$system_" = 'aarch64' ]; then
@@ -55,25 +56,30 @@ sudo pip3 install neovim
 sudo npm install neovim
 
 sudo apt install -y zsh
+
+echo 'export EDITOR="nvim"'                     | tee -a ~/.zshrc
+echo 'alias nv="nvim"'                          | tee -a ~/.zshrc
+echo 'alias vim="nvim"'                         | tee -a ~/.zshrc
+echo 'alias vi="nvim"'                          | tee -a ~/.zshrc
+echo 'alias nvi="nvim"'                         | tee -a ~/.zshrc
+echo 'alias nvimrc="cd ~/.config/nvim"'         | tee -a ~/.zshrc
+echo 'alias cat="batcat"'                       | tee -a ~/.zshrc
+echo 'alias exa="exa --icons"'                  | tee -a ~/.zshrc
+echo 'alias tre="exa --long --tree --level=3"'  | tee -a ~/.zshrc
+echo 'alias ls="exa -s type"'                   | tee -a ~/.zshrc
+echo 'alias ll="exa -all -l"'                   | tee -a ~/.zshrc
+echo 'alias d="docker"'                         | tee -a ~/.zshrc
+echo 'alias fd="fdfind"'                        | tee -a ~/.zshrc
+echo 'alias top="bpytop"'                       | tee -a ~/.zshrc
+
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo 'export EDITOR="nvim"' |tee -a ~/.zshrc | tee -a ~/.zshrc
-echo 'alias nv="nvim"' | tee -a ~/.zshrc
-echo 'alias vim="nvim"' | tee -a ~/.zshrc
-echo 'alias vi="nvim"' | tee -a ~/.zshrc
-echo 'alias nvi="nvim"' | tee -a ~/.zshrc
-echo 'alias nvimrc="cd ~/.config/nvim"' | tee -a ~/.zshrc
-echo 'alias cat="batcat"' | tee -a ~/.zshrc
-echo 'alias exa="exa --icons"' | tee -a ~/.zshrc
-echo 'alias tre="exa --long --tree --level=3"' | tee -a ~/.zshrc
-echo 'alias ls="exa -s type"' | tee -a ~/.zshrc
-echo 'alias ll="exa -all -l"' | tee -a ~/.zshrc
-echo 'alias d="docker"' | tee -a ~/.zshrc
-echo 'alias fd="fdfind"' | tee -a ~/.zshrc
-echo 'alias top="bpytop"' | tee -a ~/.zshrc
+
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting sudo)/g' ~/.zshrc
-sed -i  's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+
+cd ~/.config/nvim/shell/
 cp .p10k.zsh ~
 echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' | tee -a ~/.zshrc
 # 安装 Oh My Zsh
