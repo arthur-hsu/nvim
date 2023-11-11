ecb99f6d9573ae6084324a22ab5559a79837a1f8
arthur-mac <aaa890177@gmail.com>
2023-11-11 09:13:27 +0800

    add export PATH=$PATH:~/.local/bin

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/sh
 git config --global credential.helper store
-git config --global user.name $USER
-git config --global user.email $(hostname)
+git config --global user.name "$USER"
+git config --global user.email "$(hostname)"
 git config --global pull.rebase true
 
 rm -r ~/Bookshelf
-hostname=$(hostname)
 
 sudo cp -r ../JetBrainsMono /usr/share/fonts
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
-cd ~/Documents
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=16
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
-sudo apt-get install nodejs -y
+cd ~/Documents&&\
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg&&\
+NODE_MAJOR=16&&\
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list&&\
+sudo apt-get update&&\
+sudo apt-get install nodejs -y&&\
 
 
 sudo apt-get install -y ninja-build gettext cmake unzip curl bpytop;
@@ -78,7 +77,7 @@ echo 'alias d="docker"'                         | tee -a ~/.zshrc
 echo 'alias dc="docker-compose"'                | tee -a ~/.zshrc
 echo 'alias fd="fdfind"'                        | tee -a ~/.zshrc
 echo 'alias top="bpytop"'                       | tee -a ~/.zshrc
-
+echo 'export PATH=$PATH:~/.local/bin'           | tee -a ~/.zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
