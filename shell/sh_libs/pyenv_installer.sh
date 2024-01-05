181dbcde197415ffa8fb4ca8147b146693fe389c
arthur-mac <aaa890177@gmail.com>
2024-01-05 11:05:09 +0800

    update

--- shell/sh_libs/pyenv_installer.sh
+++ shell/sh_libs/pyenv_installer.sh
@@ -3,7 +3,18 @@ curl https://pyenv.run | bash
 pyenv update
 cat <<'EOF' | tee -a ~/.zshrc
 export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 source $(pyenv root)/completions/pyenv.zsh
+EOF
+
+# Check if ~/.zprofile exists. If not, create it.
+if [ ! -f ~/.zprofile ]; then
+    touch ~/.zprofile
+fi
+
+# Append/Update PATH and pyenv init command to ~/.zprofile
+cat <<'EOF' | tee -a ~/.zprofile
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
 EOF
