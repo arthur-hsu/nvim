05e026cd4432c9f4ead889d07e02ab1bf3724750
arthur-mac <aaa890177@gmail.com>
2024-01-08 23:45:16 +0800

    update

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,8 +1,8 @@
 return{
     {
-        "jose-elias-alvarez/null-ls.nvim",
+        "https://github.com/nvimtools/none-ls.nvim",
         lazy = true,
-        enabled = false,
+        -- enabled = false,
         event = { "BufReadPre", "BufNewFile" },
         dependencies = { "mason.nvim",'nvim-lua/plenary.nvim'},
         opts = function()
@@ -13,12 +13,43 @@ return{
                 sources = {
                     null_ls.builtins.formatting.black,
                     null_ls.builtins.formatting.prettier,
-                    
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.markdownlint,
+                    -- null_ls.builtins.formatting.jq,
+
                     -- null_ls.builtins.diagnostics.eslint,
                     -- null_ls.builtins.diagnostics.pylint,
                     -- null_ls.builtins.completion.spell,
                 },
             }
         end,
+        config = function ()
+            local lsp_formatting = function(bufnr)
+                vim.lsp.buf.format({
+                    filter = function(client)
+                        -- apply whatever logic you want (in this example, we'll only use null-ls)
+                        return client.name == "null-ls"
+                    end,
+                    bufnr = bufnr,
+                })
+            end
+
+            -- if you want to set up formatting on save, you can use this as a callback
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+            -- add to your shared on_attach callback
+            local on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            lsp_formatting(bufnr)
+                        end,
+                    })
+                end
+            end
+        end
     },
 }
