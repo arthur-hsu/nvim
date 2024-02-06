cf11d52c0695070769f0bebe897293c57ad63062
arthur-mac <aaa890177@gmail.com>
2024-02-06 14:16:46 +0800

    update

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -8,12 +8,24 @@ return {
         config = function()
             require('todo-comments').setup({
                 keywords = {
-                    tag = { icon = "󰌕 ", color = "tag" },
+                    tag = { icon = "󰌕 ", color = "tag",alt = { "RAW" } },
+                    FIX = {
+                        icon = " ", -- icon used for the sign, and in search results
+                        color = "error", -- can be a hex color, or a named color (see below)
+                        alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+                        -- signs = false, -- configure signs for some keywords individually
+                    },
+                    TODO = { icon = " ", color = "info" },
+                    HACK = { icon = " ", color = "warning" },
+                    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+                    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+                    NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+                    TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
                 },
                 signs = false,
                 sign_priority = 8, -- sign priority`
                 highlight = {
-                    multiline = true, -- enable multine todo comments
+                    multiline = false, -- enable multine todo comments
                     multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
                     multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
                     before = "fg", -- "fg" or "bg" or empty
