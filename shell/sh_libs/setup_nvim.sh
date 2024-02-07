2ae1c1300c9e0c15bd0a1cbbecda4e3dd2e53328
rak <pi5>
2024-02-07 14:06:20 +0800

    up

--- shell/sh_libs/setup_nvim.sh
+++ shell/sh_libs/setup_nvim.sh
@@ -8,7 +8,7 @@ git config --global user.email "$(hostname)"
 rm -r ~/Bookshelf ~/Music ~/Pictures ~/Public ~/Templates ~/Videos
 
 mkdir nvim_deb
-sudo rm -r /usr/share/fonts
+sudo rm -r /usr/share/fonts/JetBrainsMono
 sudo cp -r ./JetBrainsMono /usr/share/fonts
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
