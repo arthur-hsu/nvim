17607563de63d074ba42c5aad585d9bc18d7a2c7
arthur-mac <aaa890177@gmail.com>
2024-03-01 18:46:18 +0800

    update python requirments

--- lua/plugins/align.lua
+++ lua/plugins/align.lua
@@ -3,6 +3,7 @@ return {
         'https://github.com/junegunn/vim-easy-align',
         event = 'VeryLazy',
         config = function ()
+            -- Delimiter key (a single keystroke; <Space>, =, :, ., |, &, #, ,) or an arbitrary regular expression followed by <CTRL-X>
             vim.cmd([[
             xmap ga <Plug>(EasyAlign)
             nmap ga <Plug>(EasyAlign)
@@ -10,7 +11,8 @@ return {
         end
     },
     {
-        'godlygeek/tabular',
+        'https://github.com/godlygeek/tabular',
+        enable = false,
         event = 'VeryLazy',
     },
 }
