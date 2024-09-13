9d3914ee28e2e1e81365a43e71606f517f58c555
arthur-mac <aaa890177@gmail.com>
2024-09-13 11:31:46 +0800

    feat(autopair): 添加多個內部配對規則 在 autopair 插件中添加了多個內部配對規則，包括括號、引號、 Markdown 和 Python 的多行字符串等。

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -17,6 +17,22 @@ return{
                 -- multi=false,
                 tabout={multi = true},
                 pair_cmap=false,
+                internal_pairs={-- *ultimate-autopair-pairs-default-pairs*
+                    {'[',']',fly=true,dosuround=true,newline=true,space=true},
+                    {'(',')',fly=true,dosuround=true,newline=true,space=true},
+                    {'{','}',fly=true,dosuround=true,newline=true,space=true},
+                    {'"','"',suround=true,multiline=false},
+                    {"'","'",suround=true,cond=function(fn) return not fn.in_lisp() or fn.in_string() end,alpha=true,nft={'tex'},multiline=false},
+                    {'`','`',cond=function(fn) return not fn.in_lisp() or fn.in_string() end,nft={'tex'},multiline=false},
+                    {'``',"''",ft={'tex'}},
+                    {'```','```',newline=true,ft={'markdown'}},
+                    {'<!--','-->',ft={'markdown','html'},space=true},
+                    {'"""','"""',newline=true,ft={'python'}},
+                    {"'''","'''",newline=true,ft={'python'}},
+                    {'``','``',fly=true,dosuround=true,newline=false,space=true, ft={'python'}},
+                },
+
+
             })
 
             local function ls_name_from_event(event)
