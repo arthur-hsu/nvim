26dd6e5b7b13f1bac0d853bf9418bf2d83691f23
arthur.hsu <aaa890177@gmail.com>
2024-10-15 15:12:39 +0800

    feat: 更新 align 和 autopair 插件配置 - 啟用 align.lua 中的 easy_align_ignore_groups 設置 - 更新 autopair.lua 中 ultimate-autopair 插件配置 - 簡化 autopair.lua 中 confirm_done 事件處理代碼 - 調整 multi-line.lua 中 enabled 設置的格式

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -11,52 +11,36 @@ return{
         'altermo/ultimate-autopair.nvim',
         event={'InsertEnter','CmdlineEnter'},
         branch='development',
+        -- branch='v0.6',
         -- enabled=false,
         config = function ()
             require('ultimate-autopair').setup({
-                -- multi=false,
-                tabout={multi = true},
+                {'``','``',fly=true,dosuround=true,newline=true,space=false, ft={'python'}},
                 pair_cmap=false,
-                internal_pairs={-- *ultimate-autopair-pairs-default-pairs*
-                    {'[',']',fly=true,dosuround=true,newline=true,space=true},
-                    {'(',')',fly=true,dosuround=true,newline=true,space=true},
-                    {'{','}',fly=true,dosuround=true,newline=true,space=true},
-                    {'"','"',suround=true,multiline=false},
-                    {"'","'",suround=true,cond=function(fn) return not fn.in_lisp() or fn.in_string() end,alpha=true,nft={'tex'},multiline=false},
-                    {'`','`',cond=function(fn) return not fn.in_lisp() or fn.in_string() end,nft={'tex'},multiline=false},
-                    {'``',"''",ft={'tex'}},
-                    {'```','```',newline=true,ft={'markdown'}},
-                    {'<!--','-->',ft={'markdown','html'},space=true},
-                    {'"""','"""',newline=true,ft={'python'}},
-                    {"'''","'''",newline=true,ft={'python'}},
-                    {'``','``',fly=true,dosuround=true,newline=false,space=true, ft={'python'}},
+                tabout = {
+                    enable = true,
+                    hopout = true,
                 },
-
-
             })
 
-            local function ls_name_from_event(event)
-                return event.entry.source.source.client.config.name
-            end
+            local function ls_name_from_event(event) return event.entry.source.source.client.config.name end
             local cmp=require('cmp')
             local Kind=cmp.lsp.CompletionItemKind
             -- Add parenthesis on completion confirmation
             cmp.event:on(
-            'confirm_done',
-            function(event)
-                local ok, ls_name = pcall(ls_name_from_event, event)
-                -- vim.print(ls_name)
+                'confirm_done',
+                function(event)
+                    local ok, ls_name = pcall(ls_name_from_event, event)
+                    if ok and (ls_name == 'rust-analyzer' or ls_name == 'lua_ls') then
+                        return
+                    end
 
-                if ok and (ls_name == 'rust-analyzer' or ls_name == 'lua_ls') then
-                    return
+                    local completion_kind = event.entry:get_completion_item().kind
+                    if vim.tbl_contains({ Kind.Function, Kind.Method }, completion_kind) then
+                        local left = vim.api.nvim_replace_termcodes('<Left>', true, true, true)
+                        vim.api.nvim_feedkeys('()' .. left, 'n', false)
+                    end
                 end
-
-                local completion_kind = event.entry:get_completion_item().kind
-                if vim.tbl_contains({ Kind.Function, Kind.Method }, completion_kind) then
-                    local left = vim.api.nvim_replace_termcodes('<Left>', true, true, true)
-                    vim.api.nvim_feedkeys('()' .. left, 'n', false)
-                end
-            end
             )
         end
     },
@@ -98,10 +82,10 @@ return{
                 local ok, ls_name = pcall(ls_name_from_event, event)
                 -- vim.print(ls_name)
 
+                cmp_autopairs.on_confirm_done()
                 if ok and (ls_name == 'rust-analyzer' or ls_name == 'lua_ls') then
                     return
                 end
-                cmp_autopairs.on_confirm_done()
             end)
         end
     }
