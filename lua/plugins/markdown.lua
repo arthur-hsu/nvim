4b3d9123464d29dc21f91981b8566b991c8b7c4c
Arthur <aaa890177@gmail.com>
2025-01-16 10:03:45 +0800

    update

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -17,7 +17,7 @@ return {
     {
         "tadmccorkle/markdown.nvim",
         ft = { "markdown" },
-        config = function ()
+        config = function()
             require("markdown").setup()
         end
     },
@@ -25,23 +25,23 @@ return {
         "SCJangra/table-nvim",
         ft = { "markdown" },
         opts = {
-            padd_column_separators = true,        -- Insert a space around column separators.
-            mappings = {                          -- next and prev work in Normal and Insert mode. All other mappings work in Normal mode.
-                -- next = '<TAB>',                
-                -- prev = '<S-TAB>',              
-                next                = '<A-]>',    -- Go to next cell.
-                prev                = '<A-[>',    -- Go to previous cell.
-                insert_row_up       = '<A-k>',    -- Insert a row above the current row.
-                insert_row_down     = '<A-j>',    -- Insert a row below the current row.
-                move_row_up         = '<A-S-k>',  -- Move the current row up.
-                move_row_down       = '<A-S-j>',  -- Move the current row down.
-                insert_column_left  = '<A-h>',    -- Insert a column to the left of current column.
-                insert_column_right = '<A-l>',    -- Insert a column to the right of current column.
-                move_column_left    = '<A-S-h>',  -- Move the current column to the left.
-                move_column_right   = '<A-S-l>',  -- Move the current column to the right.
-                insert_table        = '<A-t>',    -- Insert a new table.
-                insert_table_alt    = '<A-S-t>',  -- Insert a new table that is not surrounded by pipes.
-                delete_column       = '<A-d>',    -- Delete the column under cursor.
+            padd_column_separators = true, -- Insert a space around column separators.
+            mappings = {                   -- next and prev work in Normal and Insert mode. All other mappings work in Normal mode.
+                -- next = '<TAB>',
+                -- prev = '<S-TAB>',
+                next                = '<A-]>',   -- Go to next cell.
+                prev                = '<A-[>',   -- Go to previous cell.
+                insert_row_up       = '<A-k>',   -- Insert a row above the current row.
+                insert_row_down     = '<A-j>',   -- Insert a row below the current row.
+                move_row_up         = '<A-S-k>', -- Move the current row up.
+                move_row_down       = '<A-S-j>', -- Move the current row down.
+                insert_column_left  = '<A-h>',   -- Insert a column to the left of current column.
+                insert_column_right = '<A-l>',   -- Insert a column to the right of current column.
+                move_column_left    = '<A-S-h>', -- Move the current column to the left.
+                move_column_right   = '<A-S-l>', -- Move the current column to the right.
+                insert_table        = '<A-t>',   -- Insert a new table.
+                insert_table_alt    = '<A-S-t>', -- Insert a new table that is not surrounded by pipes.
+                delete_column       = '<A-d>',   -- Delete the column under cursor.
             }
         }
     },
@@ -67,18 +67,18 @@ return {
             vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { fg = "#15C19E", bg = "#182931" })
             vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { fg = "#BA99F7", bg = "#29273A" })
             vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { fg = "#9B80D0", bg = "#262336" })
-            vim.api.nvim_set_hl(0, "RenderMarkdownH1",   { fg = "#76A2F6" })
-            vim.api.nvim_set_hl(0, "RenderMarkdownH2",   { fg = "#DEB06D" })
-            vim.api.nvim_set_hl(0, "RenderMarkdownH3",   { fg = "#9DCD66" })
-            vim.api.nvim_set_hl(0, "RenderMarkdownH4",   { fg = "#15C19E" })
-            vim.api.nvim_set_hl(0, "RenderMarkdownH5",   { fg = "#BA99F7" })
-            vim.api.nvim_set_hl(0, "RenderMarkdownH6",   { fg = "#9B80D0" })
-            vim.api.nvim_set_hl(0, "MarkdownH1",         { fg = "#76A2F6" })
-            vim.api.nvim_set_hl(0, "MarkdownH2",         { fg = "#DEB06D" })
-            vim.api.nvim_set_hl(0, "MarkdownH3",         { fg = "#9DCD66" })
-            vim.api.nvim_set_hl(0, "MarkdownH4",         { fg = "#15C19E" })
-            vim.api.nvim_set_hl(0, "MarkdownH5",         { fg = "#BA99F7" })
-            vim.api.nvim_set_hl(0, "MarkdownH6",         { fg = "#9B80D0" })
+            vim.api.nvim_set_hl(0, "RenderMarkdownH1", { fg = "#76A2F6" })
+            vim.api.nvim_set_hl(0, "RenderMarkdownH2", { fg = "#DEB06D" })
+            vim.api.nvim_set_hl(0, "RenderMarkdownH3", { fg = "#9DCD66" })
+            vim.api.nvim_set_hl(0, "RenderMarkdownH4", { fg = "#15C19E" })
+            vim.api.nvim_set_hl(0, "RenderMarkdownH5", { fg = "#BA99F7" })
+            vim.api.nvim_set_hl(0, "RenderMarkdownH6", { fg = "#9B80D0" })
+            vim.api.nvim_set_hl(0, "MarkdownH1", { fg = "#76A2F6" })
+            vim.api.nvim_set_hl(0, "MarkdownH2", { fg = "#DEB06D" })
+            vim.api.nvim_set_hl(0, "MarkdownH3", { fg = "#9DCD66" })
+            vim.api.nvim_set_hl(0, "MarkdownH4", { fg = "#15C19E" })
+            vim.api.nvim_set_hl(0, "MarkdownH5", { fg = "#BA99F7" })
+            vim.api.nvim_set_hl(0, "MarkdownH6", { fg = "#9B80D0" })
             vim.api.nvim_set_hl(0, "RenderMarkdownCode", { bg = "#1d1f26" })
             require('render-markdown').setup(opts)
         end
@@ -93,7 +93,7 @@ return {
                     apikey        = 'c88b2f2193424aa23e2b6f870d544176', -- Api key, required for online saving
                     local_path    = '/screenshot/',                     -- The path to put local files in, ex ~/Projects/<name>/assets/images/<imgname>.png
                     -- save          = 'online',                           -- Either 'local' or 'online'
-                    save          = 'local',                           -- Either 'local' or 'online'
+                    save          = 'local',                            -- Either 'local' or 'online'
                 },
                 ft = {                                                  -- Custom snippets for different filetypes, will replace $IMG$ with the image url
                     html     = '<img src="$IMG$" alt="">',
@@ -106,7 +106,12 @@ return {
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = function() vim.fn["mkdp#util#install"]() end,
+        -- build = function() vim.fn["mkdp#util#install"]() end,
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
         config = function()
             vim.keymap.set('n', '<leader>md', "<CMD>MarkdownPreviewToggle<CR>", { noremap = true, silent = true })
             vim.g.mkdp_auto_close        = true
@@ -118,5 +123,26 @@ return {
             vim.g.mkdp_page_title        = "${name}"
             vim.g.mkdp_theme             = 'dark'
         end
-    }
+    },
+    {
+        "toppair/peek.nvim",
+        event = { "VeryLazy" },
+        build = function()
+            local platform = vim.loop.os_uname().sysname
+            -- local tmpdir = vim.fn.stdpath("cache")
+            if platform == "Windows_NT" then
+                vim.cmd("!irm https://deno.land/install.ps1 | iex")
+            else
+                vim.cmd("!curl -fsSL https://deno.land/install.sh | sh")
+            end
+            return "deno task --quiet build:fast"
+        end,
+        config = function()
+            require("peek").setup()
+            vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+            vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+        end,
+    },
+
+
 }
