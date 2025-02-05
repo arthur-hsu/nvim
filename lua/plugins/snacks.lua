80a267b3384db6b65437a312c57d8f8c031835bb
arthur <ZT200400-NB01>
2025-02-05 13:21:59 +0800

    feat(dashboard): 新增查看提交歷史的功能 - 在儀表板中新增了一個選項，允許用戶查看提交歷史 - 新增的選項圖標為 ""，快捷鍵為 "h" - 新增的選項描述為 "Commit history" - 新增的選項動作為 ":DiffviewFileHistoryToggle %"

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -66,6 +66,7 @@ local dashboard = {
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
             { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = " ", key = "v", desc = "View change", action = ":DiffviewToggle" },
+            { icon = " ", key = "h", desc = "Commit history", action = ":DiffviewFileHistoryToggle %" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
             { icon = " ", key = "s", desc = "Restore Session" --[[ , section = "session" ]], action = ":lua require('persistence').load()" },
