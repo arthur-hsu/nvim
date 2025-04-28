faa9fd2e31cc2c2e581f2ed51f56dfc75f653573
arthur <arthur@arthurdeMacBook-Pro.local>
2025-04-29 00:38:55 +0800

    feat(plugin): 新增 dropbar 啟用邏輯與調整格式 - 對 diffview 的參數進行重新對齊，提升程式碼可讀性。 - 在 dropbar 設定中新增 enable 函式，輸出偵錯資料並依緩衝、   視窗有效性、檔案大小與語法解析狀況等條件決定是否啟用。

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -75,13 +75,13 @@ return {
 		vim.cmd("au BufWinEnter diffview://*/log/*/commit_log nnoremap <buffer> q     <Cmd>q<CR>")
 		vim.cmd("au BufWinEnter diffview://*/log/*/commit_log nnoremap <buffer> <esc> <Cmd>q<CR>")
 		require("diffview").setup({
-			diff_binaries = false, -- Show diffs for binaries
-			enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
-			git_cmd = { "git" }, -- The git executable followed by default args.
-			hg_cmd = { "hg" }, -- The hg executable followed by default args.
-			use_icons = true, -- Requires nvim-web-devicons
-			show_help_hints = true, -- Show hints for how to open the help panel
-			watch_index = true, -- Update views and index buffers when the git index changes.
+			diff_binaries    = false,      -- Show diffs for binaries
+			enhanced_diff_hl = true,       -- See ':h diffview-config-enhanced_diff_hl'
+			git_cmd          = { "git" },  -- The git executable followed by default args.
+			hg_cmd           = { "hg" },   -- The hg executable followed by default args.
+			use_icons        = true,       -- Requires nvim-web-devicons
+			show_help_hints  = true,       -- Show hints for how to open the help panel
+			watch_index      = true,       -- Update views and index buffers when the git index changes.
 			icons = { -- Only applies when use_icons is true.
 				folder_closed = "",
 				folder_open = "",
--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -9,6 +9,36 @@ return {
     },
     opts = {
         bar = {
+            enable = function(buf, win, _)
+                print("buf, win", buf, win)
+                print(vim.api.nvim_win_get_option(win, "diff"))
+                if
+                    not vim.api.nvim_buf_is_valid(buf)
+                    or not vim.api.nvim_win_is_valid(win)
+                    or vim.fn.win_gettype(win) ~= ""
+                    or vim.wo[win].winbar ~= ""
+                    or vim.bo[buf].ft == "help"
+                    or vim.bo[buf].buftype == "nowrite"
+                    -- or vim.api.nvim_win_get_option(win, "diff")
+
+                    -- or vim.bo[buf].buftype == ""
+                then
+                    return false
+                end
+
+                local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+                if stat and stat.size > 1024 * 1024 then
+                    return false
+                end
+
+                return vim.bo[buf].ft == "markdown"
+                    or pcall(vim.treesitter.get_parser, buf)
+                    or not vim.tbl_isempty(vim.lsp.get_clients({
+                        bufnr = buf,
+                        method = "textDocument/documentSymbol",
+                    }))
+            end,
+
             sources = function(buf, _)
                 local sources = require("dropbar.sources")
                 local utils = require('dropbar.utils')
