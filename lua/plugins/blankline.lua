2bc30a373985113345557ab2cd9b4294aae65d99
arthur <administrators-MacBook-Air.local>
2025-06-17 16:35:36 +0800

    chore: remove obsolete LSP configs & unused files - 刪除 lua/plugins/.tool-versions 檔案，移除不必要的版本設定。 - 調整 lua/plugins/blankline.lua 中 RainbowYellow 的處理順序。 - 刪除 lua/plugins/lspconfig_v2.lua 及   lua/plugins/mason_lspconfig_v2/lspconfig_v1.lua，以清理舊有設定。

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -7,8 +7,8 @@ local highlight = {
     "RainbowCyan",
 }
 vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#CB85E0" })
-vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
 vim.api.nvim_set_hl(0, "RainbowBlue",   { fg = "#61AFEF" })
+vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
 vim.api.nvim_set_hl(0, "RainbowGreen",  { fg = "#98C379" })
 vim.api.nvim_set_hl(0, "RainbowCyan",   { fg = "#56B6C2" })
 vim.api.nvim_set_hl(0, "RainbowRed",    { fg = "#E06C75" })
