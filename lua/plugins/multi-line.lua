7655b25b802a76db0b8728bd4b8a616fb8687272
arthur <arthur-pc>
2025-08-24 03:08:24 +0800

    feat(multi-line): add mouse support
    Enable mouse mappings and add keybindings for cursor, word, and column selection using the mouse with vim-visual-multi.

--- lua/plugins/multi-line.lua
+++ lua/plugins/multi-line.lua
@@ -2,6 +2,11 @@ return {
     'mg979/vim-visual-multi',
     event="VeryLazy",
     -- enabled = false,
+    keys = {
+        { "<C-LeftMouse>"    , mode = { "n" }, "<Plug>(VM-Mouse-Cursor)", desc = "add cursor at clicked position" },
+        { "<C-RightMouse>"   , mode = { "n" }, "<Plug>(VM-Mouse-Word)", desc = "select clicked word" },
+        { "<M-C-RightMouse>" , mode = { "n" }, "<Plug>(VM-Mouse-Column)", desc = "create column of cursors" },
+    },
     init = function()
         vim.g.VM_maps = {
             -- ["I BS"]      = "",
@@ -12,8 +17,9 @@ return {
     config = function ()
         local g = vim.g
         g.VM_skip_empty_lines               = 1
-        g.VM_set_statusline                 = 0
-        g.VM_silent_exit                    = 1
-        g.VM_quit_after_leaving_insert_mode = 1
+        g.vm_set_statusline                 = 0
+        g.vm_silent_exit                    = 1
+        g.vm_quit_after_leaving_insert_mode = 1
+        g.vm_mouse_mappings                 = 1
     end
 }
